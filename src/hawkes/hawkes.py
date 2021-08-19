b2a93b5c2549ed2b30f6fc29b44908fc9e8c0575
kimianoorbakhsh <kimianoorbakhsh@gmail.com>
2021-08-19 09:32:45 +0430

    refactored the hawkes code.

--- src/hawkes/hawkes.py
+++ src/hawkes/hawkes.py
@@ -5,8 +5,8 @@ import seaborn as sns
 import os
 import sys
 sys.path.append(os.path.abspath('..'))
+from counterfactual_tpp import sample_counterfactual
 from sampling_utils import thinning_T
-
 sns.set_style('ticks')
 
 
@@ -55,6 +55,7 @@ def sampleHawkes(lambda_0, alpha_0, w, T, Nev, seed=None):
 
     return tev, Tend, lambdas
 
+
 def iterative_sampling(all_events, events, mu0, alpha, w, lambda_max, maxNev, T):
     """Generates samples from a Hawkes process with \mu_0 and \alpha_0 until one of the following happens:
       - The next generated event is after T
@@ -69,9 +70,72 @@ def iterative_sampling(all_events, events, mu0, alpha, w, lambda_max, maxNev, T)
         for t_i in accepted_events:
             def f(t): return alpha * np.exp(-w * (t - t_i))
             new_sample, indicators = thinning_T(t_i, f, lambda_max, maxNev, T)
-            new_events = {new_sample[i]: indicators[i] for i in range(len(new_sample))}
+            new_events = {new_sample[i]: indicators[i]
+                          for i in range(len(new_sample))}
             all_events[t_i] = new_events
-            iterative_sampling(all_events, new_events, mu0, alpha, w, lambda_max, maxNev, T)
+            iterative_sampling(all_events, new_events, mu0,
+                               alpha, w, lambda_max, maxNev, T)
+
+
+def extract_samples(all_events, sampled_events, mu0, alpha, w):
+    # extracts all sampled events from all_events dictionary and also returns their intensity value.
+    all_samples = []
+    for events in list(all_events.values()):
+        all_samples.extend(list(events.keys()))
+    all_samples.sort()
+    all_samples = np.array(all_samples)
+    all_lambdas = [hawkes_calculate(
+        x, sampled_events, mu0, alpha, w) for x in all_samples]
+    all_lambdas = np.array(all_lambdas)
+    return all_samples, all_lambdas
+
+
+def sample_counterfactual_superposition(mu0, alpha, new_mu0, new_alpha, all_events, lambda_max, maxNev, w, T):
+    """Generates samples from the counterfactual intensity, and return the counterfactuals.
+    This is done in 3 steps:
+        1. First we calculate the counterfactual basedon the history in each exponential (created by superposiiton.).
+        2. Then we determine the events that were rejected in original intensity and accepted in intevened intensity (rej_acc).
+        3. Then we create a new exponential and sample for each rej_acc event.
+    """
+    def constant1(x): return mu0
+    def constant2(x): return new_mu0
+    count = 0
+    counterfactuals = {}
+    for t_i, events in all_events.items():
+        sample = list(events.keys())
+        if count == 0:
+            lambdas = [constant1(s) for s in sample]
+            counterfactuals_events, counterfactual_indicators = sample_counterfactual(
+                sample, lambdas, lambda_max, list(events.values()), constant2)
+        else:
+            def f(t): return alpha * np.exp(-w * (t - t_i))
+            def g(t): return new_alpha * np.exp(-w * (t - t_i))
+            lambdas = [f(s) for s in sample]
+            counterfactuals_events, counterfactual_indicators = sample_counterfactual(
+                sample, lambdas, lambda_max, list(events.values()), g)
+        count += 1
+        counterfactuals.update(
+            {sample[s]: counterfactual_indicators[s] for s in range(len(sample))})
+
+    rej_acc_events = {}
+    for events in list(all_events.values()):
+        for t_i in list(events.keys()):
+            if events[t_i] == False and counterfactuals[t_i] == True:
+                rej_acc_events[t_i] = True
+
+    new_events = {}
+    iterative_sampling(new_events, rej_acc_events, new_mu0,
+                       new_alpha, w, lambda_max, maxNev, T)
+    # These are the additional counterfactuals sampled from the new intensity.
+    sampled_counterfactuals = list(new_events.keys())
+    sampled_counterfactuals.sort()
+
+    # Combine all counterfactuals
+    real_counterfactuals = [k for k, v in counterfactuals.items() if v == True]
+    real_counterfactuals.extend(sampled_counterfactuals)
+    real_counterfactuals.sort()
+    real_counterfactuals = list(dict.fromkeys(real_counterfactuals))
+    return real_counterfactuals
 
 
 def hawkes(tev, l_0, alpha_0, w):
